b6f14456c0702dc416b18c18363a3e4a9314a103
Vickeysvibe <vickeydvk4@gmail.com>
2025-03-25 22:37:49 +0530

    integration start

--- Talk2Db/src/App.jsx
+++ Talk2Db/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- Talk2Db/src/Components/ChatArea/ChatArea.jsx
+++ Talk2Db/src/Components/ChatArea/ChatArea.jsx
@@ -3,7 +3,12 @@ import "./ChatArea.css";
 import InputBox from "./ChatComp/InputBox";
 import Suggestions from "./ChatComp/Suggestion";
 import Messages from "./ChatComp/Messages";
+import useApiRequest from "../../Services/useApiRequest";
+import { useNavigate, useParams } from "react-router-dom";
 const ChatArea = ({ sidebarOpen }) => {
+  const { request, loading, error, data } = useApiRequest();
+  const navigate = useNavigate();
+  const { id } = useParams();
   const [value, setValue] = useState("");
   const [messages, setMessages] = useState([
     // {
@@ -13,22 +18,53 @@ const ChatArea = ({ sidebarOpen }) => {
     // },
   ]);
 
-  const handleSend = () => {
+  const handleSend = async () => {
     if (value.trim() !== "") {
       const newMessage = {
         id: new Date().getTime(),
         text: value,
         sender: "user",
       };
-      const newMessage1 = {
+      setMessages([...messages, newMessage]);
+      const body = {
+        query: value,
+        conversation_id: id,
+      };
+      const queryResponse = await request("/generate_sql", "POST", body);
+      console.log(queryResponse);
+      const dataResponse = await request("/execute_query", "POST", {
+        query: queryResponse.sql,
+      });
+      console.table(dataResponse);
+      const botMessage = {
         id: new Date().getTime(),
-        text: "sollu da loosu bunda",
+        data: dataResponse,
+        query: queryResponse,
         sender: "bot",
       };
-      setMessages([...messages, newMessage, newMessage1]);
-      setValue("");
+      setMessages([...messages, botMessage]);
+      if (!id) {
+        navigate(`/${queryResponse.conversation_id}`);
+      }
     }
   };
+
+  // const handleSend = () => {
+  //   if (value.trim() !== "") {
+  //     const newMessage = {
+  //       id: new Date().getTime(),
+  //       text: value,
+  //       sender: "user",
+  //     };
+  //     const newMessage1 = {
+  //       id: new Date().getTime(),
+  //       text: "sollu da loosu bunda",
+  //       sender: "bot",
+  //     };
+  //     setMessages([...messages, newMessage, newMessage1]);
+  //     setValue("");
+  //   }
+  // };
 
   return (
     <div className={`ChatArea ${sidebarOpen ? "" : "sidebar-closed"}`}>
